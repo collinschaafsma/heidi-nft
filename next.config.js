ce0439ed63f177888d2daae7f9aa9fc8c4cd8d47
Collin Schaafsma <schaafsma@gmail.com>
2022-04-13 20:44:47 -0600

    ref policy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+]
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['gateway.ipfscdn.io'],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
 
